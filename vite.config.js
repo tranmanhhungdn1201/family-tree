7c5fccf66669ac43d2916c12af6580c386d7fcf4
tranmanhhung <hung.tran@enablestartup.com>
2023-04-24 13:56:28 +0700

    Commit missing

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 import { createRequire } from 'node:module';
 const require = createRequire( import.meta.url );
 export default defineConfig( {
-  base: process.env.NODE_ENV === 'production'
-      ? '/family-tree/' // Thay tên repository của các bạn vào đây nhé
-      : '/',
+  base: /family-tree/,
   plugins: [
     vue(),
     ckeditor5( { theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' ) } )
