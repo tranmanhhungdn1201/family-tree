181ff8407063072a1922d3da10cd33b8d84423f3
tranmanhhung <hung.tran@enablestartup.com>
2023-04-25 10:38:51 +0700

    Update css

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
 
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <img src="src/assets/gia-pha-ho-hua.png" class="mb-5" width="400" height="80">
+          <img src="/src/assets/gia-pha-ho-hua.png" class="mb-5" width="400" height="80">
           <div class="card shadow-2-strong header-card" style="border-radius: 1rem;">
             <div class="card-body p-5 text-center">
               <h3 class="mb-5">Đăng nhập</h3>
